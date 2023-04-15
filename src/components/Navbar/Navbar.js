98b867751164ea7b31257e048464059690bad649
eyekavya <kavyasrivastava7830@gmail.com>
2023-04-16 01:04:26 +0530

    added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { StyledNavbar } from "./StyledNavbar";
+// import "font-awesome/css/font-awesome.min.css";
 
 function Navbar() {
   return (
-    <>
+    <StyledNavbar>
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid d-flex justify-content-between">
-            <a className="navbar-brand" href="/">
+            <a className="navbar-brand title" href="/">
               Cheesy Slice
             </a>
-            <div>Cart</div>
+            <div>
+              <i class="fa-solid fa-cart-shopping"></i>
+            </div>
           </div>
         </nav>
       </div>
-    </>
+    </StyledNavbar>
   );
 }
 
---
+++ src/components/Navbar/StyledNavbar.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledNavbar = styled.section`
+  .title {
+    font-family: DynaPuff, cursive;
+    color: #fcbd3f;
+    font-size: 42px;
+    font-weight: 700;
+  }
+`;
