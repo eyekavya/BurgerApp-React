92a5eef7183695e2b7cfa0406f053af6365d5f47
eyekavya <kavyasrivastava7830@gmail.com>
2023-04-16 01:50:19 +0530

    modified navbar style

--- src/components/Navbar/StyledNavbar.js
+++ src/components/Navbar/StyledNavbar.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
 export const StyledNavbar = styled.section`
+  .nav {
+    background-color: #fcbd3f;
+    border: 1px solid #000;
+  }
+
   .title {
     font-family: DynaPuff, cursive;
-    color: #fcbd3f;
+    color: #fff;
     font-size: 42px;
     font-weight: 700;
+  }
+
+  .cart {
+    font-size: 26px;
+    margin: 1rem;
+    cursor: pointer;
   }
 `;
