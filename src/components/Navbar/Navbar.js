88d62be2d2ff75baf53bed8305cf2783402684cf
eyekavya <kavyasrivastava7830@gmail.com>
2023-04-16 01:49:55 +0530

    modified navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,13 +6,13 @@ function Navbar() {
   return (
     <StyledNavbar>
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg nav">
           <div className="container-fluid d-flex justify-content-between">
             <a className="navbar-brand title" href="/">
               Cheesy Slice
             </a>
             <div>
-              <i class="fa-solid fa-cart-shopping"></i>
+              <i class="fa-solid fa-cart-shopping cart"></i>
             </div>
           </div>
         </nav>
