fe0fa94408a285adb51d8faf7184ef666da16b56
eyekavya <kavyasrivastava7830@gmail.com>
2023-04-21 16:38:21 +0530

    added topping section

---
+++ src/components/Topping/StyledTopping.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledTopping = styled.section`
+  .flex-topping {
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    gap: 3rem;
+  }
+`;
--- src/components/Topping/Topping.js
+++ src/components/Topping/Topping.js
@@ -1,12 +1,113 @@
 import React from "react";
+import { StyledTopping } from "./StyledTopping";
 
 function Topping() {
   return (
-    <>
+    <StyledTopping>
       <div className="wrapper">
-        <h3>Select Toppings</h3>
+        <h3 className="sub-heading">Select Toppings</h3>
+        <div className="flex-topping">
+          <div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="paneer"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Paneer
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="corn"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Corn
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="onion"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Onion
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="tomato"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Tomato
+              </label>
+            </div>
+          </div>
+          <div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="mushroom"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Mushroom
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="redpepper"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Red Pepper
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="capsicum"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Capsicum
+              </label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="jalapeno"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Jalapeno
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </StyledTopping>
   );
 }
 
