00a99aa73461d0ce188ef3c7ad61ecde74a443e9
eyekavya <kavyasrivastava7830@gmail.com>
2023-04-24 00:33:38 +0530

    added topping object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 import Navbar from "./components/Navbar/Navbar.js";
 import Tagline from "./components/Tagline/Tagline.js";
 import Pizza from "./components/Pizza/Pizza.js";
-import "./App.css";
 import Topping from "./components/Topping/Topping.js";
 import Size from "./components/Size/Size.js";
 import Cheese from "./components/Cheese/Cheese.js";
 import Tab from "./components/Tab/Tab.js";
 
 function App() {
+  const [toppings, setToppings] = useState({
+    paneer: false,
+    corn: false,
+    onion: false,
+    tomato: false,
+    mushroom: false,
+    redpepper: false,
+    capsicum: false,
+    jalapeno: false,
+  });
   return (
     <>
       <div>
@@ -17,7 +27,7 @@ function App() {
           <div className="content-section">
             <Tagline />
             <Tab />
-            <Topping />
+            <Topping toppings={toppings} setToppings={setToppings} />
             <Size />
             <Cheese />
           </div>
