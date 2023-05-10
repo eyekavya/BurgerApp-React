3b27ebcddff2abacc2bc6764d93f2026a15e949d
eyekavya <kavyasrivastava7830@gmail.com>
2023-05-11 03:23:33 +0530

    added cart theme

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,8 +4,10 @@ import { StyledCart } from "./StyledCart";
 function Cart() {
   return (
     <StyledCart>
-      <div>
-        <p>Cart</p>
+      <div className="wrapper">
+        <div class="container">
+          <h1>Crust Me, You Knead This Pizza!</h1>
+        </div>
       </div>
     </StyledCart>
   );
--- src/components/Cart/StyledCart.js
+++ src/components/Cart/StyledCart.js
@@ -1,15 +1,36 @@
 import styled from "styled-components";
 
 export const StyledCart = styled.section`
-  .nav {
-    background-color: #fcbd3f;
-    border: 1px solid #000;
+  body {
+    font-family: monospace;
   }
 
-  .title {
-    font-family: DynaPuff, cursive;
-    color: #fff;
-    font-size: 38px;
-    font-weight: 700;
+  .container {
+    border-top-left-radius: 37px 140px;
+    border-top-right-radius: 23px 130px;
+    border-bottom-left-radius: 110px 19px;
+    border-bottom-right-radius: 120px 24px;
+
+    display: block;
+    position: relative;
+    border: solid 3px #6e7491;
+    padding: 40px 60px;
+    max-width: 800px;
+    width: 70%;
+    margin: 100px auto 0;
+    font-size: 17px;
+    line-height: 28px;
+    transform: rotate(-1deg);
+    box-shadow: 3px 15px 8px -10px rgba(0, 0, 0, 0.3);
+    transition: all 0.13s ease-in;
+  }
+
+  .container:hover {
+    transform: translateY(-10px) rotate(1deg);
+    box-shadow: 3px 15px 8px -10px rgba(0, 0, 0, 0.3);
+  }
+
+  .container:hover {
+    transform: translateY(4px) rotate(-5deg);
   }
 `;
