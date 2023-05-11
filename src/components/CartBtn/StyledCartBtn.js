f8870e5813b87ec086545452dd547cfc4fa7530e
eyekavya <kavyasrivastava7830@gmail.com>
2023-05-11 21:11:36 +0530

    refactored

--- src/components/CartBtn/StyledCartBtn.js
+++ src/components/CartBtn/StyledCartBtn.js
@@ -1,15 +1,3 @@
 import styled from "styled-components";
 
-export const StyledCartBtn = styled.section`
-  .addToCartBtn {
-    font-family: "Poppins", sans-serif;
-    font-size: 22px;
-    width: 12rem;
-    background-color: #fcbd3f;
-    color: #000;
-    font-weight: 500;
-    border: 1.5px solid #000;
-    border-radius: 20px;
-    padding: 4px;
-  }
-`;
+export const StyledCartBtn = styled.section``;
